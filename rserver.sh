c1d1260cc9d6d637108b546b729864ab5e7e6703
Pharmlovex <pharmifedayoojo@gmail.com>
2022-11-08 15:26:12 +0000

    update

--- rserver.sh
+++ rserver.sh
@@ -9,3 +9,12 @@ sudo apt-get --assume-yes install r-base
 sudo apt-get --assume-yes install gdebi-core
 wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
 sudo gdebi --non-interactive rstudio-server-1.1.383-amd64.deb
+
+
+
+
+# rstudio docker 
+docker pull rocker/tidyverse
+
+docker run -d --name rstudio -v $HOME:/singlecell/SingleCellAnalysis -e PASSWORD=Lovex -p 8787:8787 rocker/tidyverse
+
