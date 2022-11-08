a8edb25d34ba7b588a2ec6408ba65d25087e0022
pharmlovex <pharmifedayoojo@gmail.com>
2022-11-08 14:18:18 +0000

    update script

---
+++ rserver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sudo apt-get --assume-yes update
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt-get --assume-yes install r-base
+sudo apt-get --assume-yes install gdebi-core
+wget https://download2.rstudio.org/rstudio-server-1.1.383-amd64.deb
+sudo gdebi --non-interactive rstudio-server-1.1.383-amd64.deb
